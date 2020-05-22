dd34ea317ac356dc90d61da57a7b5a1bb0717f97
the-maux <maxime.renaud@epitech.eu>
2020-05-22 16:06:05 +0200

    C.I(codacy) fix docker build on dev

--- src/manage.py
+++ src/manage.py
@@ -1,39 +1,39 @@
 import os
 import sys
-# import subprocess
-# import time
+import subprocess
+import time
 
 
-# def startRedisAgain(isTest):
-#     try:
-#         output = subprocess.check_output(['redis-cli', 'ping'])
-#         if 'PONG' in str(output):
-#             print('[LOG] Redis detected and running -> OK', flush=True)
-#             return True
-#         elif 'No such file or directory:' in str(output):
-#             print("[ERROR] can't execute redis")
-#         print(output)
-#     except FileNotFoundError:
-#         if isTest is True:
-#             return True
-#         print("[ERROR] Redis is not installed")
-#     return False
-#
-#
-# def redis_sanity_check(isTest):
-#     try:
-#         return startRedisAgain(isTest)
-#     except subprocess.CalledProcessError:
-#         print('[LOG] Trying to start Redis mannualy\n$>', flush=True)
-#         subprocess.check_output(['nohup', 'redis-server', '--protected-mode no'])
-#         time.sleep(3)
-#         if startRedisAgain(isTest):
-#             return True
-#     print('[ERROR] Redis didnt answered, is redis installed ?', flush=True)
-#     return False
+def startRedisAgain(isTest):
+    try:
+        output = subprocess.check_output(['/usr/bin/redis-cli', 'ping'])
+        if 'PONG' in str(output):
+            print('[LOG] Redis detected and running -> OK', flush=True)
+            return True
+        elif 'No such file or directory:' in str(output):
+            print("[ERROR] can't execute redis")
+        print(output)
+    except FileNotFoundError:
+        if isTest is True:
+            return True
+        print("[ERROR] Redis is not installed")
+    return False
 
 
-def startDjango(settings_path='tipboard.webserver.settings', isTest=False):
+def redis_sanity_check(isTest):
+    try:
+        return startRedisAgain(isTest)
+    except subprocess.CalledProcessError:
+        print('[LOG] Trying to start Redis mannualy\n$>', flush=True)
+        subprocess.check_output(['/usr/bin/nohup', '/usr/bin/redis-server', '--protected-mode no'])
+        time.sleep(3)
+        if startRedisAgain(isTest):
+            return True
+    print('[ERROR] Redis didnt answered, is redis installed ?', flush=True)
+    return False
+
+
+def startDjango(settings_path='tipboard.webserver.settings'):
     """ Start the django with DJANGO_SETTINGS_MODULE path added in env """
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_path)
     from django.core.management import execute_from_command_line
@@ -56,10 +56,10 @@ def main_as_pkg():
 
 if __name__ == '__main__':
     argv = sys.argv[1]
-    sys.path.insert(0, os.getcwd())  # Import project to PYTHONPATH
+    sys.path.insert(0, os.getcwd())
     if argv in ('sensors', '-s'):
         from src.sensors.sensors_main import scheduleYourSensors
         scheduleYourSensors()
     elif argv in ('test', 'runserver', 'migrate', 'shell', 'collectstatic', 'findstatic'):
-        exit(startDjango(isTest='test' in argv))
+        exit(startDjango())
     exit(show_help())
