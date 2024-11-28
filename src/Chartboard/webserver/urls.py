9b2832cf91713ada896f94550531e4e2312b43e6
10101349 <G0I63Rs>
2024-11-28 15:25:08 +0100

    uopgrade to django 4.0 url by re_path

--- src/Chartboard/webserver/urls.py
+++ src/Chartboard/webserver/urls.py
@@ -2,7 +2,7 @@ import sys
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
-from django.conf.urls import url
+from django.urls import re_path
 from src.Chartboard.app.applicationconfig import redis_sanity_check
 from src.Chartboard.app.views.api import push_api, project_info, tile_rest
 from src.Chartboard.app.views.flipboard import getDashboardsPaths, renderFlipboardHtml
@@ -11,15 +11,15 @@ from src.Chartboard.app.views.flipboard import renderDashboardHtmlUniqueDashboar
 favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
-    url(r'^flipboard/getDashboardsPaths$', getDashboardsPaths),  # get all dashboard in Config/
+    re_path(r'^flipboard/getDashboardsPaths$', getDashboardsPaths),  # get all dashboard in Config/
 
-    url(r'^api/tiledata/([a-zA-Z0-9_-]+)$', tile_rest),  # get data of a single tile
-    url(r'^api/push$', push_api),  # update data for a single tile
-    url(r'^api/info$', project_info),  # get data from the Chartboard server
+    re_path(r'^api/tiledata/([a-zA-Z0-9_-]+)$', tile_rest),  # get data of a single tile
+    re_path(r'^api/push$', push_api),  # update data for a single tile
+    re_path(r'^api/info$', project_info),  # get data from the Chartboard server
 
-    url(r'^$', renderFlipboardHtml),  # start the flipboard logic for mulCarle dashboard in a single flipboard.html
-    url(r'^([a-zA-Z0-9_-]*)$', renderDashboardHtmlUniqueDashboard),  # render a single dashboard.html
-    url(r'^dashboard/([a-zA-Z0-9_-]*)$', renderDashboardHtmlForFlipboard),  # render the tiles html for ws in client js
+    re_path(r'^$', renderFlipboardHtml),  # start the flipboard logic for mulCarle dashboard in a single flipboard.html
+    re_path(r'^([a-zA-Z0-9_-]*)$', renderDashboardHtmlUniqueDashboard),  # render a single dashboard.html
+    re_path(r'^dashboard/([a-zA-Z0-9_-]*)$', renderDashboardHtmlForFlipboard),  # render the tiles html for ws in client js
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
